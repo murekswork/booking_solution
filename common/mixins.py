07caead2a8eb118a800a1c9cf734eb21352eb90c
murekswork <murekswork@gmail.com>
2024-03-29 03:18:02 +0300

    mixin with queryset of authenticated user objects

---
+++ common/mixins.py
@@ -0,0 +1,15 @@
+class UserQuerySetMixin:
+    user_field = 'user'
+    allow_staff_view = False
+    allow_superuser_view = True
+
+    def get_queryset(self):
+        user = self.request.user
+        qs = super().get_queryset()
+        if self.allow_superuser_view is True and user.is_superuser:
+            return qs
+        if self.allow_staff_view and user.is_staff:
+            return qs
+        lookup_data = {}
+        lookup_data[self.user_field] = user
+        return qs.filter(**lookup_data)
